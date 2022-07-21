e78c15f799a4b714db172d19c037a3674afe684d
Zhi Keng He <zhikhe@gmail.com>
2022-07-21 10:32:32 -0400

    update base to support gh-pages

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,16 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import { svelte } from '@sveltejs/vite-plugin-svelte'
 import postcss from './postcss.config.js'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  css: { postcss },
-  plugins: [svelte()]
-})
+export default ({ mode }) => {
+  process.env = {...process.env, ...loadEnv(mode, process.cwd())};
+
+  console.log('Building for /', process.env.VITE_BASE_URL)
+
+  return defineConfig({
+    css: { postcss },
+    plugins: [svelte()],
+    base: process.env.VITE_BASE_URL ? `/${process.env.VITE_BASE_URL}/` : './'
+  });
+}
