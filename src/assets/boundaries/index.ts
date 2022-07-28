dc514a5c08c0c05361d776fe76ae5b3cc51be06c
Eva Decker <itsevadecker@gmail.com>
2022-07-28 15:26:47 -0400

    Add boundary descriptions

--- src/assets/boundaries/index.ts
+++ src/assets/boundaries/index.ts
@@ -22,6 +22,12 @@ export interface ILayer {
   /** Human-readable name, plural, e.g. "Community Districts" */
   name_plural: string;
 
+  /** Succinct definition of what this boundary represents. */
+  description: string;
+
+  /** Optional 'learn more' link for the description. */
+  description_url?: string;
+
   /** SQL query to fetch data from Carto */
   sql: string;
 
@@ -43,6 +49,9 @@ export const layers: ILayers = {
   cd: {
     name: 'Community District',
     name_plural: 'Community Districts',
+    description:
+      'Community Boards advise on land use and zoning, participate in the city budget process, and address service delivery in their district. They have up to 50 up to 50 volunteer members appointed by the borough president.',
+    description_url: 'https://communityprofiles.planning.nyc.gov/',
     // Remove parks
     sql: `SELECT * FROM all_bounds WHERE id = 'cd' AND NOT namecol IN ('164','226','227','228','355','356','480','481','482','483','484','595')`,
     icon: '💬',
@@ -51,6 +60,9 @@ export const layers: ILayers = {
   pp: {
     name: 'Police Precinct',
     name_plural: 'Police Precincts',
+    description:
+      'A police precinct is a geographical area patrolled by the NYPD. The term "precinct" may also refer to the main police station for such a geographical area.',
+    description_url: 'https://www1.nyc.gov/site/nypd/index.page',
     sql: `SELECT * FROM all_bounds WHERE id = 'pp'`,
     icon: '🚔',
     formatUrl: precinct => get_pp_url(parseInt(precinct)),
@@ -59,6 +71,9 @@ export const layers: ILayers = {
   dsny: {
     name: 'Sanitation District',
     name_plural: 'Sanitation Districts',
+    description:
+      'For operations management, the Department of Sanitation (DSNY) divides the city into 59 Sanitation Districts. The boundary for each district is congruent with the Community District boundary.',
+    description_url: 'https://www1.nyc.gov/assets/dsny/site/home',
     sql: `SELECT * FROM all_bounds WHERE id = 'dsny'`,
     icon: '🗑',
     formatContent: name => format_default(name)
@@ -66,6 +81,8 @@ export const layers: ILayers = {
   fb: {
     name: 'Fire Battalion',
     name_plural: 'Fire Battalions',
+    description:
+      'A Fire Battalion consists of several fire stations and multiple fire companies, and is led by a Battalion Chief.',
     sql: `SELECT * FROM all_bounds WHERE id = 'fb'`,
     icon: '🔥',
     formatContent: name => format_default(name)
@@ -73,6 +90,9 @@ export const layers: ILayers = {
   sd: {
     name: 'School District',
     name_plural: 'School Districts',
+    description:
+      'A School District is a geographical area with multiple schools.',
+    description_url: 'https://www.schools.nyc.gov/',
     sql: `SELECT * FROM all_bounds WHERE id = 'sd'`,
     icon: '🎓',
     formatContent: name => format_default(name)
@@ -80,6 +100,8 @@ export const layers: ILayers = {
   hc: {
     name: 'Health Center District',
     name_plural: 'Health Center Districts',
+    description:
+      'Health Center Districts are aggregates of health areas and are used for reporting health statistics. They were created by the NYC Department of Health and Mental Hygiene (DOHMH).',
     sql: `SELECT * FROM all_bounds WHERE id = 'hc'`,
     icon: '🩺',
     formatContent: name => format_default(name)
@@ -87,6 +109,9 @@ export const layers: ILayers = {
   cc: {
     name: 'City Council District',
     name_plural: 'City Council Districts',
+    description:
+      'The New York City Council is the lawmaking body of New York City. It has 51 members from 51 council districts throughout the five boroughs.',
+    description_url: 'https://council.nyc.gov/',
     sql: `SELECT * FROM all_bounds WHERE id = 'cc'`,
     icon: '🗽',
     formatUrl: name => `https://council.nyc.gov/district-${name}`,
@@ -95,6 +120,9 @@ export const layers: ILayers = {
   nycongress: {
     name: 'Congressional District',
     name_plural: 'Congressional Districts',
+    description:
+      'Congressional Districts are the 435 regions from which voting representatives are elected to the U.S. House of Representatives.',
+    description_url: 'https://www.house.gov/',
     sql: `SELECT * FROM all_bounds WHERE id = 'nycongress'`,
     icon: '🇺🇸',
     formatUrl: name => `https://www.govtrack.us/congress/members/NY/${name}`,
@@ -103,6 +131,9 @@ export const layers: ILayers = {
   sa: {
     name: 'State Assembly District',
     name_plural: 'State Assembly Districts',
+    description:
+      'The New York State Assembly is the lower house of the New York State Legislature, with the New York State Senate being the upper house. There are 150 seats in the Assembly.',
+    description_url: 'https://nyassembly.gov/',
     sql: `SELECT * FROM all_bounds WHERE id = 'sa'`,
     icon: '🧑‍⚖️',
     formatContent: name => format_default(name)
@@ -110,6 +141,9 @@ export const layers: ILayers = {
   ss: {
     name: 'State Senate District',
     name_plural: 'State Senate Districts',
+    description:
+      'The New York State Senate is the upper house of the New York State Legislature; the New York State Assembly is its lower house. Its members are elected to two-year terms; there are no term limits. There are 63 seats in the Senate.',
+    description_url: 'https://www.nysenate.gov/',
     sql: `SELECT * FROM all_bounds WHERE id = 'ss'`,
     icon: '⚖️',
     formatUrl: name => `https://www.nysenate.gov/district/${name}`,
@@ -118,6 +152,8 @@ export const layers: ILayers = {
   nta: {
     name: 'Neighborhood Tabulation Area',
     name_plural: 'Neighborhood Tabulation Areas',
+    description:
+      'Neighborhood Tabulation Areas are aggregations of census tracts, adapted as a method of presenting the U.S. Census Bureau’s American Community Survey (ACS).',
     sql: `SELECT * FROM all_bounds WHERE namecol NOT IN ('park-cemetery-etc-Brooklyn','park-cemetery-etc-Queens', 'park-cemetery-etc-Bronx', 'park-cemetery-etc-Manhattan', 'park-cemetery-etc-Staten Island', 'Airport') and id = 'nta'`,
     icon: '🏘',
     formatContent: name => format_default(name)
@@ -125,6 +161,9 @@ export const layers: ILayers = {
   bid: {
     name: 'Business Improvement District',
     name_plural: 'Business Improvement Districts',
+    description:
+      'A Business Improvement District (BID) is a geographical area where local stakeholders oversee and fund the maintenance, improvement, and promotion of their commercial district, going above and beyond those typically provided by the city.',
+    description_url: 'https://www1.nyc.gov/site/sbs/neighborhoods/bids.page',
     sql: `SELECT * FROM all_bounds WHERE id = 'bid'`,
     icon: '💸',
     formatContent: name => format_bid(name)
@@ -132,6 +171,8 @@ export const layers: ILayers = {
   zipcode: {
     name: 'Zipcode',
     name_plural: 'Zipcodes',
+    description:
+      'A ZIP Code is a postal code used by the United States Postal Service (USPS).',
     sql: `SELECT * FROM all_bounds WHERE id = 'zipcode'`,
     icon: '📫',
     formatContent: name => format_default(name)
--- src/components/Sidebar/BoundaryDetails.svelte
+++ src/components/Sidebar/BoundaryDetails.svelte
@@ -7,6 +7,7 @@
     hoveredDistrictId,
     mapStore
   } from '../../stores';
+  import { clickOutside } from 'svelte-use-click-outside';
   import { resetZoom, sortedDistricts } from '../../helpers/helpers';
   import DistrictLink from './DistrictLink.svelte';
   import Loader from '../Loader.svelte';
@@ -15,6 +16,7 @@
   let value = '';
   let districts: Feature[] = [];
   let isLoading: boolean;
+  let isDetailPaneOpen: boolean = false;
 
   function onDistrictMouseOver(districtId: string) {
     if ($hoveredDistrictId && $selectedBoundaryMap) {
@@ -71,28 +73,68 @@
     : 'Loading&hellip;'}
   onBack={handleBack}
 >
-  <div class="relative mt-2 -mb-1">
-    <input
-      id="filter"
-      placeholder="Filter"
-      type="search"
-      name="filter"
-      bind:value
-      autocomplete="off"
-      class="block w-full py-1 px-3 pl-10 bg-gray-100 rounded focus:outline-none focus:ring focus:ring-blue-500"
-    />
-    <svg
-      xmlns="http://www.w3.org/2000/svg"
-      class="h-5 w-5 absolute left-1.5 top-1.5"
-      viewBox="0 0 20 20"
-      fill="currentColor"
-    >
-      <path
-        fill-rule="evenodd"
-        d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z"
-        clip-rule="evenodd"
+  <div class="flex mt-2">
+    <div class="relative flex-1">
+      <input
+        id="filter"
+        placeholder="Filter"
+        type="search"
+        name="filter"
+        bind:value
+        autocomplete="off"
+        class="block w-full py-1 px-3 pl-10 bg-gray-100 rounded focus:outline-none focus:ring focus:ring-blue-500"
       />
-    </svg>
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        class="h-5 w-5 absolute left-1.5 top-1.5"
+        viewBox="0 0 20 20"
+        fill="currentColor"
+      >
+        <path
+          fill-rule="evenodd"
+          d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z"
+          clip-rule="evenodd"
+        />
+      </svg>
+    </div>
+    {#if $selectedBoundaryMap}
+      <div class="relative">
+        <button
+          use:clickOutside={() => (isDetailPaneOpen = false)}
+          on:click={() => (isDetailPaneOpen = !isDetailPaneOpen)}
+          class={`w-8 h-8 ml-2 text-lg flex justify-center items-center rounded-full text-gray-500 hover:bg-gray-100 hover:text-gray-800 ${
+            isDetailPaneOpen && 'bg-gray-100 text-gray-800'
+          }`}
+        >
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            class="h-5 w-5"
+            viewBox="0 0 20 20"
+            fill="currentColor"
+          >
+            <path
+              fill-rule="evenodd"
+              d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-7-4a1 1 0 11-2 0 1 1 0 012 0zM9 9a1 1 0 000 2v3a1 1 0 001 1h1a1 1 0 100-2v-3a1 1 0 00-1-1H9z"
+              clip-rule="evenodd"
+            />
+          </svg>
+        </button>
+        <div
+          class={`absolute top-full right-0 w-72 bg-white rounded shadow-md p-2 py-4 mb-4 px-4 text-gray-500 text-sm ${
+            isDetailPaneOpen ? 'visible' : 'hidden'
+          }`}
+        >
+          {layers[$selectedBoundaryMap].description}
+          {#if layers[$selectedBoundaryMap].description_url}
+            <a
+              href={layers[$selectedBoundaryMap].description_url}
+              class="underline"
+              target="_blank">Learn more</a
+            >
+          {/if}
+        </div>
+      </div>
+    {/if}
   </div>
 </SidebarHeader>
 <div class="py-2">
